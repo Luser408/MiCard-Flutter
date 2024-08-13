4ec74465c202ef6282327d4d0c52770ef00b58f7
Hamza Khan <hamza128482@gmail.com>
2024-08-13 11:52:10 +0500

    MiCard
    MiCard
    MiCard is a digital personal business card app that allows you to share your contact details seamlessly. Imagine needing to give someone your business card or contact details but realizing you don't have a physical card with you. MiCard solves this problem by letting others download your business card directly as an app.
    
    Features
    Digital Business Card: Store all your important contact information in one place.
    Shareable App: Share your MiCard with others quickly and easily.
    Customizable Interface: Personalize your card with your preferred colors, fonts, and details.
    Lightweight: Designed to be quick and easy to use, with minimal storage requirements.
    Screenshots
    (Add screenshots of your app here)
    
    Installation
    To install the app on your device:
    
    Clone the repository:
    bash
    Copy code
    git clone https://github.com/your-Luser408/MiCard.git
    Open the project in your preferred IDE (e.g., Android Studio).
    Run the app on an emulator or physical device.
    Usage
    Open the MiCard app.
    Enter your contact details, including name, phone number, email, and any other relevant information.
    Customize the appearance of your business card.
    Share your MiCard with others via a QR code, link, or by having them download the app.
    Built With
    Flutter - The mobile framework used
    Dart - The programming language used
    Contributing
    If you would like to contribute to the project, please fork the repository and submit a pull request. We welcome any improvements, bug fixes, or new features.
    
    License
    This project is licensed under the MIT License - see the LICENSE file for details.
    
    Contact
    For any questions or feedback, feel free to contact me:
    
    Email: your-hamza128482@gmail.com
    LinkedIn: https://www.linkedin.com/in/hamza-khan-8a6009230?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app

---
+++ apppractice/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/hack.jpg'),
+                ),
+                Text(
+                  'Luser',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '+92 3556387981',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    )),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'hamza128482@email.com',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro'),
+                      ),
+                    ))
+              ],
+            )),
+      ),
+    );
+  }
+}
